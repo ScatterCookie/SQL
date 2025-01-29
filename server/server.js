7bdfcbe2264c00ef90356546d4d5042fb025d6f4
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-29 14:56:47 +0000

    my first SQL

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+console.log(process.env.DB_CONN);
+const db = new pg.Pool({ connectionString: process.env.DB_CONN });
+
+//use statements
+// allows incoming requests from other people
+app.use(cors());
+//read incoming json
+app.use(express.json());
+//looks for .ENV and pulls the environment variable into the node process
+
+app.get("/games", async (res, req) => {
+  const result = await db.query("SELECT * FROM GAMES");
+  const games = result.rows;
+  res.json(games);
+});
+
+app.post("/games", async (req, res) => {
+  const gameFromReq = req.body.game;
+  const platformFronReq = req.body.platform;
+  const data = await db.query(
+    `INSERT INTO games (platform, game) VALUES ('${gameFromReq}', '${platformFronReq}')`
+  );
+  res.json(data);
+});
+
+app.listen("8080", () => {
+  console.log("app running on port 8080! http://localhost:8080");
+});
