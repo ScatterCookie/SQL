504f9ad8f79b58ddba80c7dbf8e4b48d553141c5
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-01-30 12:34:26 +0000

    still trying

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,17 @@
-const messageForm = document.getElementById("message-form");
+const gamesForm = document.getElementById("games-form");
 
 function handleSubmitMessageForm(event) {
   event.preventDefault();
 
   const formData = new FormData(messageForm);
-  const message = formData.get("message");
+  const games = formData.get("games");
 
-  fetch("https://sql-6009.onrender.com/messages", {
+  fetch("https://sql-6009.onrender.com/games", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(message),
+    body: JSON.stringify(games),
   });
 }
 
